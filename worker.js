6bc913491e0d084c6e1d34e85e0a84709cdcd4cb
wsalqlyzadh0-hub <wsalqlyzadh0@gmail.com>
2026-08-22 19:48:13 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,12 +2,18 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/api/test") {
-      return new Response("Worker OK", {
-        headers: {
-          "Content-Type": "text/plain; charset=UTF-8"
+    if (url.pathname === "/api/test-secret") {
+      return new Response(
+        JSON.stringify({
+          username: Boolean(env.ADMIN_USERNAME),
+          password: Boolean(env.ADMIN_PASSWORD)
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
         }
-      });
+      );
     }
 
     return env.ASSETS.fetch(request);
